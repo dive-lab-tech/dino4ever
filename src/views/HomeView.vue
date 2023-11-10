c785b46cf1e0d7dba5ee110b30141685c571a509
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2023-11-09 18:00:35 -0600

    Refactor CSS styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,10 +11,19 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 let urlNeonText = ref(neonText);
 
+let scrollBottom = ref(false);
+
+
 setTitle("Dinosaurios Recargados");
 
 //animación de opacidad y escala en los elementos del banner
 onMounted(() => {
+  const divPadre = document.getElementById('prueba'); 
+  const alturaDivPadre = divPadre.offsetHeight;
+  divPadre.style.marginTop = '-'+alturaDivPadre/2 + 'px';
+  console.log(alturaDivPadre);
+
+
   gsap.to(".banner-dino", {
     duration: 1,
     opacity: 1,
@@ -37,6 +46,19 @@ onMounted(() => {
     },
   });
 
+  //Validacion de scroll Y
+  window.addEventListener("scroll", () => {
+    const scroll = window.scrollY;
+    if (scroll > 100) {
+      scrollBottom.value = true;
+    }
+    else{
+      scrollBottom.value = false;
+    }
+  });
+
+
+  
   // Animación de texto en bucle
   (function animateNeonText() {
     const tl = gsap.timeline({
@@ -70,7 +92,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="font-montserrat"> 
+  <div class="font-montserrat">
     <!-- <div class="h-screen overflow-hidden banner-dino" style="opacity: 0">
       <div class="bg-home w-full h-full">
         <div class="flex flex-col h-full w-full pt-28 justify-between">
@@ -98,19 +120,47 @@ onMounted(() => {
     </div> -->
 
     <div class="w-full">
-      <div class=" bg-white banner-dino-fit">
-        <img src="@/assets/images/banner-home-sec-1.jpg" alt="" class=" max-[768px]:hidden">
+      <div class=" bg-white banner-dino-fit" >
+        <img src="@/assets/images/banner-home-sec-1.jpg" alt="" class=" max-[768px]:hidden" style="">
         <img src="@/assets/images/banner-home-sec-mobile-1.jpg" alt="" class="hidden max-[767px]:block">
+        
+      </div>
+    </div>
+    <div class="w-full text-white ">
+      <div class=" w-full ">
+        <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 bg-primary bg-opacity-23  px-10 py-10">
+          <div class="lg:col-span-2 " :class="scrollBottom == true ? 'flex items-center animate-center' : ''">
+  <div class="h-auto">
+    <h1 class="font-montserrat-bold text-6xl uppercase title-ticket">Compra</h1>
+    <h2 class="font-montserrat-bold text-6xl uppercase title-ticket">tus <span class="ml-1">entradas</span></h2>
+    <h4 class="font-montserrat px-3 py-2">
+      Del 1 de diciembre al 16 de junio del 2024.
+    </h4>
+  </div>
+</div>
+
+          <div class="h-auto flex justify-center items-center">
+            <div class=" card-detail bg-slate-400">
+              
+            </div>
+          </div>
+        </div>
+        <div class="flex justify-end w-full" id="prueba">
+          <div class=" w-4/12 bg-accent py-2 rounded-l-lg"> 
+            <h3 class=" font-montserrat-bold text-2xl text-center uppercase " style="text-shadow: rgb(0 255 255) 1px 0px 15px;">No te quedes sin tus entradas</h3>
+          </div>
+        </div>
       </div>
     </div>
     <div class="h-screen max-[767px]:h-auto max-[767px]:py-10">
       <div class="h-full w-full pt-10 ">
         <div class="w-full flex justify-center items-center h-2/12 max-[767px]:my-10 ">
-          <h1 class="text-white font-extrabold text-4xl max-[767px]:text-center title-gallery font-montserrat-bold">NUESTRA DINOGALERÍA</h1>
+          <h1 class="text-primaryLight font-extrabold text-4xl max-[767px]:text-center title-gallery font-montserrat-bold">
+            NUESTRA DINOGALERÍA</h1>
         </div>
         <div class="h-10/12">
           <div class="flex justify-center items-center">
-            <div class="w-6/12 max-[767px]:w-11/12 " >
+            <div class="w-6/12 max-[767px]:w-11/12 ">
               <GalleryHome />
             </div>
           </div>
@@ -126,71 +176,79 @@ onMounted(() => {
           </div>
           <div class="h-9/12 max-[767px]:w-full max-[767px]:p-2">
             <div class="flex justify-between flex-col w-full h-full">
-                <div class="flex justify-center items-center h-full ">
-                  <div class="w-6/12 text-white text-center  text-lg min-[1500px]:text-xl max-[767px]:w-full max-[767px]:text-sm">
-                <p>
-                  ¡Prepárate para vivir una experiencia única e inolvidable en
-                  el evento más esperado del año! 
-                </p>
-                <p class="mb-3">
-                  Con más de 5000 metros
-                  cuadrados de exhibición donde podrás interactuar con
-                  dinosaurios animatronics de última generación a escala real y
-                  descubrir nuevos mundos increíbles de luces y proyecciones.
-                </p>
-                <p class="mb-3">
-                   Además, podrás disfrutar de actividades interactivas en todo
-                  el recorrido y sumergirte en áreas de realidad virtual
-                  innovadora que te llevarán a la época prehistórica como nunca
-                  antes lo habías imaginado.
-                </p>
-                <p class="mb-3">
-                  Todo esto en una puesta en escena
-                  moderna única en Chile. Llega una nueva generación recargada
-                  de dinosaurios que te transportarán a un mundo lleno de
-                  energía y emoción. 
-                </p>
-                <p class="mb-3">
-                  ¡No te pierdas esta oportunidad de vivir
-                  una aventura emocionante junto a tu familia y amigos en el
-                  centro cultural Estación Mapocho!
-                </p>
-              </div>
+              <div class="flex justify-center items-center h-full ">
+                <div
+                  class="w-6/12 text-white text-center  text-lg min-[1500px]:text-xl max-[767px]:w-full max-[767px]:text-sm">
+                  <p>
+                    ¡Prepárate para vivir una experiencia única e inolvidable en
+                    el evento más esperado del año!
+                  </p>
+                  <p class="mb-3">
+                    Con más de 5000 metros
+                    cuadrados de exhibición donde podrás interactuar con
+                    dinosaurios animatronics de última generación a escala real y
+                    descubrir nuevos mundos increíbles de luces y proyecciones.
+                  </p>
+                  <p class="mb-3">
+                    Además, podrás disfrutar de actividades interactivas en todo
+                    el recorrido y sumergirte en áreas de realidad virtual
+                    innovadora que te llevarán a la época prehistórica como nunca
+                    antes lo habías imaginado.
+                  </p>
+                  <p class="mb-3">
+                    Todo esto en una puesta en escena
+                    moderna única en Chile. Llega una nueva generación recargada
+                    de dinosaurios que te transportarán a un mundo lleno de
+                    energía y emoción.
+                  </p>
+                  <p class="mb-3">
+                    ¡No te pierdas esta oportunidad de vivir
+                    una aventura emocionante junto a tu familia y amigos en el
+                    centro cultural Estación Mapocho!
+                  </p>
                 </div>
+              </div>
               <div class="text-white flex  flex-col justify-center mb-10">
                 <div class="text-center">
                   <p class=" text-primaryLight text-2xl font-bold">MÁS RECARGADOS QUE NUNCA</p>
                 </div>
                 <div class="text-center mt-3">
-                  <a href="/faqs" class=" bg-primaryLight px-7 py-2 rounded-3xl text-black button-faqs transition duration-500">FAQs</a>
+                  <a href="/faqs"
+                    class=" bg-primaryLight px-7 py-2 rounded-3xl text-black button-faqs transition duration-500">FAQs</a>
                 </div>
               </div>
             </div>
           </div>
 
-       <FooterComponent />
+          <FooterComponent />
 
         </div>
       </div>
     </div>
 
   </div>
-
-
-
-
-
 </template>
 <style scoped>
+
+.card-detail{
+  width: 300px;
+  height: 300px;
+
+}
+.banner-dino-fit{
+  background: linear-gradient(to bottom, transparent 0%, black 100%);
+}
 .title-gallery {
-  text-shadow:  rgb(255, 255, 255) 1px 0px 15px;
+  text-shadow: rgb(255, 255, 255) 1px 0px 15px;
 }
-.button-faqs:hover{
+
+.button-faqs:hover {
   opacity: 0.8;
-  box-shadow: 0px 7px 28px 1px rgba(255,255,255,0.47);
--webkit-box-shadow: 0px 7px 28px 1px rgba(255,255,255,0.47);
--moz-box-shadow: 0px 7px 28px 1px rgba(255,255,255,0.47);
+  box-shadow: 0px 7px 28px 1px rgba(255, 255, 255, 0.47);
+  -webkit-box-shadow: 0px 7px 28px 1px rgba(255, 255, 255, 0.47);
+  -moz-box-shadow: 0px 7px 28px 1px rgba(255, 255, 255, 0.47);
 }
+
 .text-intro {
   left: 33rem;
   display: flex;
@@ -231,34 +289,31 @@ onMounted(() => {
   left: 28rem;
 }
 
- /* Pantallas grandes */
- @media (min-width: 1500px) {
-  }
-  /* PC */
-  @media (min-width: 1200px) and (max-width: 1499px) {}
-  /* Pantallas para tablets */
-  @media only screen and (min-width: 801px) and (max-width: 1199px) {
-  }
-  
-  /* Pantallas para tablets pequeñas*/
-  @media only screen and (min-width: 768px) and (max-width: 800px) {
-  }
-  
-  /* Pantallas mobiles */
-  @media only screen and (min-width: 381px) and (max-width: 767px) {
+/* Pantallas grandes */
+@media (min-width: 1500px) {}
 
-    .bg-home {
-  background-image: url("@/assets/images/bg-home-mobile.webp");
+/* PC */
+@media (min-width: 1200px) and (max-width: 1499px) {}
+
+/* Pantallas para tablets */
+@media only screen and (min-width: 801px) and (max-width: 1199px) {}
+
+/* Pantallas para tablets pequeñas*/
+@media only screen and (min-width: 768px) and (max-width: 800px) {}
+
+/* Pantallas mobiles */
+@media only screen and (min-width: 381px) and (max-width: 767px) {
+
+  .bg-home {
+    background-image: url("@/assets/images/bg-home-mobile.webp");
+  }
+
 }
-  
+
+/*Mobile con pantallas pequeñas*/
+@media (max-width: 380px) {
+  .bg-home {
+    background-image: url("@/assets/images/bg-home-mobile.webp");
   }
-  /*Mobile con pantallas pequeñas*/
-  @media (max-width: 380px) { 
-    .bg-home {
-  background-image: url("@/assets/images/bg-home-mobile.webp");
 }
-  }
-
-
-  
 </style>
